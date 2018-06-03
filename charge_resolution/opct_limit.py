62c45c2c5b8ee71e21e9594182510b90af7f52ba
Jason Watson <jason.jw@live.co.uk>
2018-06-03 11:28:59 +0100

    Created opct_limit and spe_spectrum scripts

---
+++ charge_resolution/opct_limit.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy.special import binom
+from numba import jit
+from scipy.stats import poisson, norm as gaus
+from scipy.special import factorial
+from matplotlib import pyplot as plt
+from IPython import embed
+from CHECLabPy.utils.resolutions import ChargeResolution, ChargeStatistics
+import pandas as pd
+import os
+from tqdm import tqdm
+
+
+C = np.sqrt(2.0 * np.pi)
+NPEAKS = 1000
+
+N = np.arange(NPEAKS)[:, None]
+J = np.arange(NPEAKS)[None, :]
+K = np.arange(1, NPEAKS)[:, None]
+FACTORIAL_INV = 1 / factorial(np.arange(NPEAKS))[None, :]
+BINOM = binom(N - 1, J - 1)
+
+x = np.arange(NPEAKS)
+def get_distribution(opct, lambda_):
+    pj = poisson.pmf(J, lambda_)
+    pct = np.nansum(pj * np.power(1 - opct, J) * np.power(opct, N - J) * BINOM, 1)
+    pct[0] = poisson.pmf(0, lambda_)
+    return pct
+
+
+# norm = 1
+# eped = 0
+# eped_sigma = 5.05
+# spe = 16.859
+# spe_sigma = 1.81
+# spe_sigma /= spe
+# eped_sigma /= spe
+# spe /= spe
+# x = np.linspace(-1.5, 500, 10000)
+# def get_distribution(opct, lambda_):
+#     p_ped = poisson.pmf(0, lambda_)
+#     ped_signal = norm * p_ped * gaus.pdf(x, eped, eped_sigma)
+#
+#     pj = poisson.pmf(J, lambda_)
+#     pct = np.nansum(pj * np.power(1-opct, J) * np.power(opct, N - J) * BINOM, 1)
+#
+#     sap = spe_sigma
+#
+#     pe_sigma = np.sqrt(K * spe_sigma ** 2 + eped_sigma ** 2)
+#
+#     signal = pct[K] * gaus.pdf(x, eped + K * spe, pe_sigma)
+#     signal *= norm
+#     pe_signal = np.nansum(signal, 0)
+#
+#     s = ped_signal + pe_signal
+#     s /= s.sum()
+#
+#     return s
+
+opct = 0.2
+lambda_ = 1
+
+cr = ChargeResolution()
+cs = ChargeStatistics()
+desc = "Looping over lambdas"
+true_arr = np.geomspace(0.1, 100, 100)
+for lambda_ in tqdm(true_arr, desc=desc):
+    pixel = 1920
+    dist = get_distribution(opct, lambda_)
+    n = 100000
+    measured = np.random.choice(x, p=dist, size=n)
+    true = lambda_
+    cr.add(pixel, true, measured)
+    cs.add(pixel, true, measured)
+df_pixel, _ = cr.finish()
+x = df_pixel['true']
+cr = df_pixel['rmse']
+df_pixel, _ = cs.finish()
+mean = df_pixel['mean']
+
+np.savez("opct_limit.npz", x=x, cr=cr, mean=mean)
--- charge_resolution/plot_charge_resolution.py
+++ charge_resolution/plot_charge_resolution.py
@@ -2,22 +2,28 @@ import os
 import numpy as np
 import pandas as pd
 from CHECLabPy.plotting.resolutions import ChargeResolutionPlotter, \
-    ChargeMeanPlotter
+    ChargeResolutionWRRPlotter, ChargeMeanPlotter
 
 
 def main():
     poi = 1920
 
     paths = dict(
-        withped="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_withped/charge_res.h5",
-        poly="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_poly/charge_res.h5",
-        none="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_none/charge_res.h5"
+        withped="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_withped/charge_res_0.h5",
+        poly="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_poly/charge_res_0.h5",
+        none="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_none/charge_res_0.h5",
+        withped_wb="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_withped/charge_res_wb.h5",
+        poly_wb="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_poly/charge_res_wb.h5",
+        none_wb="/Volumes/gct-jason/data_checs/dynamicrange_180514/tf_none/charge_res_wb.h5",
     )
 
     labels = dict(
         withped="TF With Pedestal",
         poly="TF Polynomial Regression",
-        none="Pedestal Only"
+        none="Pedestal Only",
+        withped_wb="TF With Pedestal, Without bias",
+        poly_wb="TF Polynomial Regression, Without bias",
+        none_wb="Pedestal Only, Without bias",
     )
 
     stores = {key: pd.HDFStore(path) for key, path in paths.items()}
@@ -30,7 +36,10 @@ def main():
     keys = [
         "withped",
         "poly",
-        "none"
+        "none",
+        # "withped_wb",
+        # "poly_wb",
+        # "none_wb"
     ]
     p_cr = ChargeResolutionPlotter()
     for key in keys:
@@ -43,22 +52,49 @@ def main():
     p_cr.plot_goal(true)
     p_cr.plot_requirement(true)
     p_cr.plot_poisson(true)
+    p_cr.plot_opct_file()
     p_cr.add_legend()
     p_cr.save(os.path.join(output_dir, "charge_res.pdf"))
 
     keys = [
         "withped",
         "poly",
-        "none"
+        "none",
+        # "withped_wb",
+        # "poly_wb",
+        # "none_wb"
+    ]
+    p_crwrr = ChargeResolutionWRRPlotter()
+    for key in keys:
+        store = stores[key]
+        label = labels[key]
+        p_crwrr.set_store(store)
+        p_crwrr.plot_camera(label + " (camera)")
+        # p_crwrr.plot_pixel(poi, label + " (pixel {})".format(poi))
+    true = np.geomspace(0.1, 2000, 1000)
+    p_crwrr.plot_goal(true)
+    p_crwrr.plot_requirement(true)
+    p_crwrr.plot_poisson(true)
+    p_crwrr.add_legend("lower right")
+    p_crwrr.save(os.path.join(output_dir, "charge_res_wrr.pdf"))
+
+    keys = [
+        "withped",
+        "poly",
+        "none",
+        # "withped_wb",
+        # "poly_wb",
+        # "none_wb"
     ]
     p_mean = ChargeMeanPlotter()
     for key in keys:
         store = stores[key]
         label = labels[key]
         p_mean.set_store(store)
-        p_mean.plot_camera(label + " (camera)")
-        # p_mean.plot_pixel(poi, label + " (pixel {})".format(poi))
-    p_mean.add_legend("upper left")
+        # p_mean.plot_camera(label + " (camera)")
+        p_mean.plot_pixel(poi, label + " (pixel {})".format(poi))
+    p_mean.plot_opct_file()
+    p_mean.add_legend("best")
     p_mean.save(os.path.join(output_dir, "charge_mean.pdf"))
 
 
